08a88ded1f5b35c35fda2cb2c68b5349999368d2
kayshad <kaisslama@gmail.com>
2023-12-26 21:34:58 +0100

    Update opsexp.py

--- kAddon/opsexp.py
+++ kAddon/opsexp.py
@@ -1,8 +1,11 @@
+import bpy
+import ast
+import mathutils
+
 class ExpressionNodeOperator(bpy.types.Operator):
     bl_idname = 'wm.add_expression_nodes'
     bl_label = 'Create Nodes'
     bl_options = {'REGISTER', 'UNDO'}
-
     value_node_cache = {}
     init_position = [100, 300]
     space_x = 100
@@ -11,11 +14,12 @@ class ExpressionNodeOperator(bpy.types.Operator):
 
 
 
-    def add_node(self, type):
-        bpy.ops.node.add_node(type=type)
-        return bpy.context.active_node
 
-    def create_binary_operator(self, value, location):
+    def add_node(self, type, node_tree):
+        node = node_tree.nodes.new(type=type)
+        return node
+
+    def create_binary_operator(self, value, location, node_tree):
         operation_map = {
             ast.Add: "ADD",
             ast.Mult: "MULTIPLY",
@@ -24,17 +28,17 @@ class ExpressionNodeOperator(bpy.types.Operator):
             ast.Mod: "MODULO",
             ast.Pow: "POWER",
         }
-        left_node = self.create_nodes(value.left, [location[0]-self.space_x, location[1]+self.space_y])
-        right_node = self.create_nodes(value.right, [location[0]-self.space_x, location[1]-self.space_y])
-        node = self.add_node("ShaderNodeMath")
+        left_node = self.create_nodes(value.left, [location[0]-self.space_x, location[1]+self.space_y], node_tree)
+        right_node = self.create_nodes(value.right, [location[0]-self.space_x, location[1]-self.space_y], node_tree)
+        node = self.add_node("ShaderNodeMath", node_tree)
         self.operator_node_list.append(node)
-        self.node_tree.links.new(node.inputs[0], left_node.outputs[0])
-        self.node_tree.links.new(node.inputs[1], right_node.outputs[0])
+        node_tree.links.new(node.inputs[0], left_node.outputs[0])
+        node_tree.links.new(node.inputs[1], right_node.outputs[0])
         node.operation = operation_map.get(type(value.op), "ADD")
         node.location = location
         return node
 
-    def create_call(self, value, location):
+    def create_call(self, value, location, node_tree):
         func_map = {
             "sin": "SINE",
             "cos": "COSINE",
@@ -68,34 +72,36 @@ class ExpressionNodeOperator(bpy.types.Operator):
         }
         arg_nodes = []
         for idx, arg in enumerate(value.args):
-            arg_nodes.append(self.create_nodes(arg, [location[0]-self.space_x, location[1]+idx * self.space_y]))
-        node = self.add_node("ShaderNodeMath")
+            arg_nodes.append(self.create_nodes(arg, [location[0]-self.space_x, location[1]+idx * self.space_y], node_tree))
+        node = self.add_node("ShaderNodeMath", node_tree)
         node.operation = func_map.get(value.func.id, "ADD")
         node.location = location
         for idx, arg in enumerate(arg_nodes):
-            self.node_tree.links.new(node.inputs[idx], arg.outputs[0])
+            node_tree.links.new(node.inputs[idx], arg.outputs[0])
+
         return node
 
-    def create_name(self, value, location):
+    def create_name(self, value, location, node_tree):
         if value.id in self.value_node_cache:
             return self.value_node_cache[value.id]
-        node = self.add_node("ShaderNodeValue")
+        node = self.add_node("ShaderNodeValue", node_tree)
         node.label = value.id
         node.location = location
         self.value_node_cache[value.id] = node
+
         return node
 
-    def create_constant(self, value, location):
-        node = self.add_node("ShaderNodeValue")
+    def create_constant(self, value, location, node_tree):
+        node = self.add_node("ShaderNodeValue", node_tree)
         node.outputs[0].default_value = value.value
         node.label = str(value.value)
         node.location = location
         return node
 
-    def create_invalid(self, *_):
+    def create_invalid(self, *_, node_tree):
         return None
 
-    def create_nodes(self, value, location):
+    def create_nodes(self, value, location, node_tree):
         types_map = {
             ast.BinOp: self.create_binary_operator,
             ast.Call: self.create_call,
@@ -103,12 +109,13 @@ class ExpressionNodeOperator(bpy.types.Operator):
             ast.Constant: self.create_constant
         }
         creator = types_map.get(type(value), self.create_invalid)
-        return creator(value, location)
+        return creator(value, location, node_tree)
 
-    def parse_expressions(self, props):
+    def parse_expressions(self, props, node_tree):
         module = ast.parse(props.expression)
         for expression in module.body:
-            root = self.create_nodes(expression.value, self.init_position)
+            root = self.create_nodes(expression.value, self.init_position, node_tree)
+
 
     def execute(self, context):
         scene = context.scene
@@ -116,5 +123,38 @@ class ExpressionNodeOperator(bpy.types.Operator):
         self.node_tree = space.node_tree
         self.value_node_cache = {}
         self.operator_node_list = []
-        self.parse_expressions(scene.custom_expression_nodes_props)
+
+        group = bpy.data.node_groups.new('EiE', 'GeometryNodeTree')
+        group.interface.new_socket("Geometry", in_out='INPUT', socket_type='NodeSocketGeometry')
+        input_node = group.nodes.new('NodeGroupInput')
+        input_node.select = False
+        input_node.location.x = -200 - input_node.width
+        group.interface.new_socket("Geometry", in_out='OUTPUT', socket_type='NodeSocketGeometry')
+        output_node = group.nodes.new('NodeGroupOutput')
+        output_node.is_active_output = True
+        output_node.select = False
+        output_node.location.x = 200 + output_node.width
+        ng = self.node_tree.nodes.new('GeometryNodeGroup')
+
+        ng.node_tree = group
+        ngt = ng.node_tree
+
+        self.parse_expressions(scene.custom_expression_nodes_props, ngt )
+
+        a = self.add_node('GeometryNodeCurvePrimitiveLine', ngt)
+        a.inputs['End'].default_value = mathutils.Vector((1.0, 2.0, 3.0))
+        a1 = self.add_node('GeometryNodeResampleCurve', ngt)
+        a1.inputs['Count'].default_value = 100
+        b = self.add_node('GeometryNodeCurveToMesh', ngt)
+        c = self.add_node('GeometryNodeCurvePrimitiveCircle', ngt)
+
+
+
+        ngt.links.new(a.outputs['Curve'],a1.inputs['Curve'])
+        ngt.links.new(a1.outputs['Curve'],b.inputs['Curve'])
+        ngt.links.new(c.outputs['Curve'],b.inputs['Profile Curve'])
+        ngt.links.new(b.outputs['Mesh'],output_node.inputs['Geometry'])
+
         return {'FINISHED'}
+
+
